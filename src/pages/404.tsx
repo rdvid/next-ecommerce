aaadc21872a1c411db7375a040191f802d6464b3
Rafael <rafael_dvid@hotmail.com>
2024-01-20 22:57:28 -0300

    change icon and creating header, footer and sidebar components

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,4 +1,4 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faMugHot } from '@fortawesome/free-solid-svg-icons';
 import Head from 'next/head';
 
@@ -11,10 +11,8 @@ export default function NotFound() {
       <div className="text-center text-red-600 align-center">
         <h1 className="mb-4 text-6xl font-semibold text-red-500">404</h1>
         <p className="mb-4 text-lg text-gray-600">Oops! Looks like you're lost.</p>
-          <div className="animate-bounce">
-            <FontAwesomeIcon icon={faMugHot}/>
-          </div>
-        <p className="mt-4 text-gray-600">Let's get you back <a href="/" className="text-blue-500">home</a>.</p>
+        <FontAwesomeIcon icon={faMugHot} className='animate-bounce'/>
+        <p className="mt-4 text-gray-600">Let's get you back <a href="/products" className="text-blue-500">home</a>.</p>
       </div>
     </main>
   );
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,9 +1,9 @@
-import Link from "../components/link/link";
+import Link from "../components/link";
 
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Link href={'/'}>
+      <Link href={'/products'}>
         <h1 className="text-red-600">back to home</h1>
       </Link>
     </main>
